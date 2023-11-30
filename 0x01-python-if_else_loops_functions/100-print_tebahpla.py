2d89864291f9fdff275eca1d531c5eebf1c6e01b
lumicodes22 <lumicodes1@gmail.com>
2023-11-29 16:05:42 -0800

    made corrections

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(i, chr(i - 33)), end="")
+
+for i in range(122, 96, -1):
+    print("{}".format(
+        chr(i).lower() if i % 2 == 0 else chr(i).upper()
+        ), end="")
