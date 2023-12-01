386469a9517ec499b4cba1f26fa9b39f323c9597
lumicodes22 <lumicodes1@gmail.com>
2023-11-30 16:04:14 -0800

    first commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from add_0 import add
+#assign value to variable a and b
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
